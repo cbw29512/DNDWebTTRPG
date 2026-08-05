31c7c3619002e1a82dc45e548eefa6f04158af45
Chris Wilson <divclass01@gmail.com>
2026-08-05 06:40:24 -0400

    Preserve card module export compatibility

--- src/wishing-cake-pack.js
+++ src/wishing-cake-pack.js
@@ -1,9 +1,12 @@
-import { wishingCakeCards } from './wishing-cake-cards.js';
+import { wishingCakeCards as legacyWishingCakeCards } from './wishing-cake-cards.js';
 import { wishingCakeSpatialCards } from './wishing-cake-spatial-cards.js';
 
 const oldContextCardIds = new Set(['location', 'room']);
 
 export const wishingCakePackCards = Object.freeze([
   ...wishingCakeSpatialCards,
-  ...wishingCakeCards.filter(card => !oldContextCardIds.has(card.id))
+  ...legacyWishingCakeCards.filter(card => !oldContextCardIds.has(card.id))
 ]);
+
+// Preserve the established manifest-module export name for future dynamic loaders.
+export const wishingCakeCards = wishingCakePackCards;
