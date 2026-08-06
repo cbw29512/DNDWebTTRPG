defcd3449fab147af07401ecb313e421fb5a2795
Chris Wilson <divclass01@gmail.com>
2026-08-06 00:06:33 -0400

    Apply audited Wishing Cake rules over legacy cards

--- src/wishing-cake-pack.js
+++ src/wishing-cake-pack.js
@@ -1,11 +1,17 @@
 import { wishingCakeCards as legacyWishingCakeCards } from './wishing-cake-cards.js';
 import { wishingCakeSpatialCards } from './wishing-cake-spatial-cards.js';
+import { wishingCakeAuditedRules } from './wishing-cake-audited-rules.js';
 
 const oldContextCardIds = new Set(['location', 'room']);
-
-export const wishingCakePackCards = Object.freeze([
+const baseCards = [
   ...wishingCakeSpatialCards,
   ...legacyWishingCakeCards.filter(card => !oldContextCardIds.has(card.id))
+];
+const auditedById = new Map(wishingCakeAuditedRules.map(card => [card.id, card]));
+
+export const wishingCakePackCards = Object.freeze([
+  ...baseCards.map(card => auditedById.get(card.id) ?? card),
+  ...wishingCakeAuditedRules.filter(card => !baseCards.some(base => base.id === card.id))
 ]);
 
 // Preserve the established manifest-module export name for future dynamic loaders.
